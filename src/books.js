48eab7c0e329135c82223f37ed7997841fadf5d3
JackWatt89 <jackwatt@live.co.uk>
2021-10-07 09:45:40 +0100

    React Tutorial

---
+++ src/books.js
@@ -0,0 +1,30 @@
+export const books = [
+
+{
+  id: 1,
+  img:'https://images-eu.ssl-images-amazon.com/images/I/71f5EShRDbS._AC_UL200_SR200,200_.jpg',
+  title: 'The Thursday Murder Club',
+  author: 'Jack Watt'
+}
+,
+{
+  id: 2,
+  img:'https://images-eu.ssl-images-amazon.com/images/I/81LME3qQp7S._AC_UL200_SR200,200_.jpg',
+  title: 'Beautiful World',
+  author: 'Sally Rooney'
+}
+,
+{
+  id: 3,
+  img:'https://images-eu.ssl-images-amazon.com/images/I/91FdP9%2BU7HL._AC_UL200_SR200,200_.jpg',
+  title: 'Family Comforts',
+  author: 'Rebecca Wilson'
+}
+,
+{
+  id: 4,
+  img:'https://images-eu.ssl-images-amazon.com/images/I/718MlWuZCqL._AC_UL200_SR200,200_.jpg',
+  title: 'The Midnight Library',
+  author: 'Matt Haig'
+}
+ ];
